0209be72a2550668f685fa84ab24222777652965
Bailey Townsend <baileytownsend2323@gmail.com>
2025-04-07 22:25:10 -0500

    We can find them now

--- esquema-cli/src/main.rs
+++ esquema-cli/src/main.rs
@@ -1,14 +1,27 @@
 use atrium_api::agent::atp_agent::AtpAgent;
 use atrium_api::agent::atp_agent::store::MemorySessionStore;
+use atrium_api::agent::bluesky::AtprotoServiceType;
+use atrium_api::did_doc::Service;
 use atrium_api::types::LimitedNonZeroU8;
-use atrium_api::types::string::AtIdentifier::Did as AtIdentifierDid;
-use atrium_api::types::string::Did as DidType;
+use atrium_api::types::string::AtIdentifier::{Did as AtIdentifierDid, Handle};
+use atrium_api::types::string::{AtIdentifier, Did as DidType};
+use atrium_common::resolver::Resolver;
+
+use atrium_identity::{
+    did::{CommonDidResolver, CommonDidResolverConfig, DEFAULT_PLC_DIRECTORY_URL},
+    handle::{AtprotoHandleResolver, AtprotoHandleResolverConfig, DnsTxtResolver},
+};
+use atrium_oauth::DefaultHttpClient;
 use atrium_xrpc_client::reqwest::ReqwestClient;
 use clap::{Parser, Subcommand};
 use esquema_codegen::genapi;
+use hickory_resolver::TokioAsyncResolver;
+use log::info;
+use std::error::Error;
 use std::fs;
 use std::path::PathBuf;
-use atrium_identity::identity_resolver::IdentityResolver;
+use std::str::FromStr;
+use std::sync::Arc;
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -39,7 +52,8 @@ struct Generate {
 enum GenerateCommands {
     /// Generates rust types from your local json lexicon files
     Local(LocalGenerate),
-    Remote(DidGenerate),
+    /// Generates rust types from a remote pds repository
+    Remote(RepoGenerate),
 }
 
 #[derive(Parser, Debug)]
@@ -77,41 +91,80 @@ fn local_generate_action(args: &LocalGenerate) -> Result<(), Box<dyn std::error:
     name = "remote",
     about = "Generates Rust types from remote Lexicon ATProto records"
 )]
-struct DidGenerate {
-    /// The remote DID to check against
-    #[arg(short, long)]
-    did: String,
+struct RepoGenerate {
+    /// The owner of the PDS repo
+    #[arg(long)]
+    handle: String,
     /// Namespace for recursion. Example xyz.statusphere will check for schemas under that collection, but xyz.statusphere.graphs will just check under the .graphs of xyz.statusphere
     #[arg(short, long)]
-    namespace: PathBuf,
-    //TODO i don't think pds is proper here?
-    /// The url for your PDS, like https://public.api.bsky.app
-    #[arg(short, long, default_value = "https://public.api.bsky.app")]
-    pds: String,
+    namespace: String,
+    /// The collection that holds the lexicon schema, it is usually 'com.atproto.lexicon.schema'
+    #[arg(short, long, default_value = "com.atproto.lexicon.schema")]
+    collection: String,
 }
 
-async fn did_generate_action(args: &DidGenerate) -> Result<(), Box<dyn std::error::Error>> {
-    let agent = AtpAgent::new(
-        ReqwestClient::new(args.pds.as_str()),
-        MemorySessionStore::default(),
-    );
-    let parsed_did = AtIdentifierDid(DidType::new(args.did.clone())?);
-    //Im not sure atrium can do this? may be a manual thing for now?
-    // let identity = IdentityResolver::new()
-    // let did_doc = agent.api.com.atproto.identity.
+async fn did_generate_action(args: &RepoGenerate) -> Result<(), Box<dyn std::error::Error>> {
+    // Currently just constructing in this command but may move to an app state with DI?
+    let http_client = Arc::new(DefaultHttpClient::default());
+    let did_resolver = CommonDidResolver::new(CommonDidResolverConfig {
+        plc_directory_url: DEFAULT_PLC_DIRECTORY_URL.to_string(),
+        http_client: Arc::clone(&http_client),
+    });
+
+    let handle_resolver = AtprotoHandleResolver::new(AtprotoHandleResolverConfig {
+        dns_txt_resolver: HickoryDnsTxtResolver::default(),
+        http_client: Arc::clone(&http_client),
+    });
+
+    let did = handle_resolver
+        .resolve(&atrium_api::types::string::Handle::from_str(
+            args.handle.as_str(),
+        )?)
+        .await?;
+
+    let resolved_did = did_resolver.resolve(&did).await?;
+    let pds_url = resolved_did
+        .service
+        .as_ref()
+        .and_then(|services| {
+            services
+                .iter()
+                .find(|service| service.r#type == "AtprotoPersonalDataServer")
+                .map(|service| service.service_endpoint.clone())
+        })
+        .ok_or_else(|| "No valid PDS URL found for this DID")?; // Return error if not found
 
     //This endpoint needs your PDS endpoint, for example mine is "https://coral.us-east.host.bsky.network"
-    let repos = agent
+    let agent = AtpAgent::new(ReqwestClient::new(pds_url), MemorySessionStore::default());
+
+    let records = agent
         .api
         .com
         .atproto
         .repo
-        .describe_repo(
-            atrium_api::com::atproto::repo::describe_repo::ParametersData { repo: parsed_did }
-                .into(),
+        .list_records(
+            atrium_api::com::atproto::repo::list_records::ParametersData {
+                collection: args.collection.parse()?,
+                cursor: None,
+                limit: None,
+                repo: AtIdentifier::Did(did.clone()),
+                reverse: None,
+            }
+            .into(),
         )
         .await?;
-    println!("{:?}", repos);
+
+    let record_uri_prefix = format!(
+        "at://{}/{}/{}",
+        did.as_str(),
+        args.collection,
+        args.namespace,
+    );
+    for ref record in &records.records {
+        if record.uri.starts_with(record_uri_prefix.as_str()) {
+            println!("Found it: {:?}", record);
+        }
+    }
 
     Ok(())
 }
@@ -124,5 +177,33 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             GenerateCommands::Local(args) => local_generate_action(args),
             GenerateCommands::Remote(args) => did_generate_action(args).await,
         },
+    }
+}
+
+struct HickoryDnsTxtResolver {
+    resolver: TokioAsyncResolver,
+}
+
+impl Default for HickoryDnsTxtResolver {
+    fn default() -> Self {
+        Self {
+            resolver: TokioAsyncResolver::tokio_from_system_conf()
+                .expect("failed to create resolver"),
+        }
+    }
+}
+
+impl DnsTxtResolver for HickoryDnsTxtResolver {
+    async fn resolve(
+        &self,
+        query: &str,
+    ) -> core::result::Result<Vec<String>, Box<dyn Error + Send + Sync + 'static>> {
+        Ok(self
+            .resolver
+            .txt_lookup(query)
+            .await?
+            .iter()
+            .map(|txt| txt.to_string())
+            .collect())
     }
 }
