d11ae2b7c440c85d890e7a8360338ab2b1b79458
Jamie Blondin <jblondin@gmail.com>
2025-05-22 21:26:04 -0400

    fixed bad genapi call example build.rs

--- esquema-example/build.rs
+++ esquema-example/build.rs
@@ -7,5 +7,5 @@ fn main() {
     let out_dir = std::env::var("OUT_DIR").expect("OUT_DIR environment variable must be specified");
     let lex_dir = PathBuf::from("./lexicons");
     let output = PathBuf::from(out_dir);
-    let _ = genapi(&lex_dir, &output).unwrap();
+    let _ = genapi(&lex_dir, &output, &None).unwrap();
 }
